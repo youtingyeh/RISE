12487ac5af5e77a5803a3d0b7264c0a3f81681fa
youtingyeh <youtingyeh@gmail.com>
2026-09-11 16:22:17 +0800

    Add initial auth configuration settings

---
+++ auth-config.js
@@ -0,0 +1,9 @@
+// 只填公開的 Project URL 與 publishable key／anon key。
+// 不可填 service_role、secret key、SMTP 密碼。
+window.RISE_AUTH_CONFIG = Object.freeze({
+  url: "",
+  publishableKey: "",
+  siteURL: "https://youtingyeh.github.io/RISE/",
+  // 團隊完成個資告知內容後填入網址；未填前不開放註冊。
+  privacyURL: ""
+});
